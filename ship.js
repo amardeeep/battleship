97a5191f16df3a918e55a8b4dffde79ffc0794bd
Deetsy <amardeepdeepak2016@gmail.com>
2024-06-10 12:23:39 +0530

    finish ship that has length and hit and isSunk properties

--- ship.js
+++ ship.js
@@ -1,4 +1,21 @@
-function ship() {
-  return "ship";
+function ship(length) {
+  const lengthOfShip = length;
+  let hits = 0;
+  function hit() {
+    hits++;
+    return hits;
+  }
+  function isSunk() {
+    if (hits == lengthOfShip) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return {
+    lengthOfShip,
+    hit,
+    isSunk,
+  };
 }
 export { ship };
--- ship.test.js
+++ ship.test.js
@@ -1,5 +1,11 @@
 import expect from "expect";
 import { ship } from "./ship.js";
-test("ship", () => {
-  expect(ship()).toBe("ship");
+test("ship length", () => {
+  expect(ship(5).lengthOfShip).toBe(5);
+});
+test("hit", () => {
+  expect(ship(7).hit()).toBe(1);
+});
+test("isSunk", () => {
+  expect(ship(4).isSunk()).toBe(false);
 });
