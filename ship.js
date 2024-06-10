0141c5ebe3c044d65181de5f0d15f4019d12104e
Deetsy <amardeepdeepak2016@gmail.com>
2024-06-10 09:01:41 +0530

    Setup jest and babel

---
+++ ship.js
@@ -0,0 +1,4 @@
+function ship() {
+  return "ship";
+}
+export { ship };
---
+++ ship.test.js
@@ -0,0 +1,5 @@
+import expect from "expect";
+import { ship } from "./ship.js";
+test("ship", () => {
+  expect(ship()).toBe("ship");
+});
