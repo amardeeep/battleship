ee737711cd80e857545406967c180e9af563bd2b
Deetsy <amardeepdeepak2016@gmail.com>
2024-06-11 23:18:23 +0530

    add recieve attack fucntion

--- gameBoard.js
+++ gameBoard.js
@@ -1,9 +1,10 @@
+import { co } from "co";
 import { ship } from "./ship.js";
 function gameBoard() {
   let board = [];
   let ships = [
     { name: "Destroyer", shipObject: ship(5) },
-    { name: " WarShip", shipObject: ship(3) },
+    { name: "WarShip", shipObject: ship(3) },
   ];
   //create board to store
   for (let i = 0; i < 10; i++) {
@@ -23,44 +24,33 @@ function gameBoard() {
   //function to place a ship at given coordinates
   function placeShip(row, column, axis, shipname) {
     let ship = getShip(shipname);
-    if ((axis = "x")) {
-      if (column + ship.lengthOfShip < 10) {
-        for (let temp = 0; temp <= ship.lengthOfShip; temp++) {
+    let length = ship.lengthOfShip;
+    if (axis == "x") {
+      if (column + length < 10) {
+        for (let temp = 0; temp <= length; temp++) {
           board[row][column + temp].isShip = shipname;
         }
       }
     }
-    if ((axis = "y")) {
-      if (row + ship.lengthOfShip < 10) {
-        for (let temp = 0; temp <= ship.lengthOfShip; temp++) {
+    if (axis == "y") {
+      if (row + length < 10) {
+        for (let temp = 0; temp <= length; temp++) {
           board[row + temp][column].isShip = shipname;
         }
       }
     }
     return board;
   }
-  return { board, placeShip };
+  //fucntion recieveAttack
+  function recieveAttack(row, column) {
+    if (board[row][column].isShip == "none") {
+      board[row][column].isAttacked = "Miss";
+    } else {
+      board[row][column].isAttacked = "hit";
+      let hitShip = getShip(board[row][column].isShip);
+      return hitShip.hit();
+    }
+  }
+  return { board, placeShip, recieveAttack };
 }
 export { gameBoard };
-/*function placeShip(i, j, axis, ship, length) {
-    if (axis == "x") {
-      if (10 - length >= j) {
-        for (let temp = 0; temp < length; temp++) {
-          board[i][temp + j].isShip = true;
-          board[i][temp + j].shipName = ship;
-        }
-      } else {
-        return "Invalid coordinates!";
-      }
-    } else if (axis == "y") {
-      if (10 - length >= i) {
-        for (let temp = 0; temp < length; temp++) {
-          board[temp + i][j].isShip = true;
-          board[temp + i][j].shipName = ship;
-        }
-      } else {
-        return "Invalid Coordinates!";
-      }
-    }
-    return board;
-  }*/
--- gameBoard.test.js
+++ gameBoard.test.js
@@ -1,18 +1,24 @@
-const { default: expect } = require("expect");
+import expect from "expect";
 import { gameBoard } from "./gameBoard.js";
 import { ship } from "./ship.js";
 test("gameboard is a 10 by 10 matrix", () => {
-  expect(gameBoard().board.length).toBe(10),
-    expect(gameBoard().board[0].length).toBe(10);
+  expect(gameBoard().board.length).toBe(10);
+  expect(gameBoard().board[0].length).toBe(10);
   expect(gameBoard().board[5][5].isShip).toBe("none");
 });
+let board = gameBoard();
+board.placeShip(3, 3, "x", "Destroyer");
+board.placeShip(2, 2, "y", "WarShip");
 test("ships are placed at some coordinates", () => {
-  let board = gameBoard();
-  board.placeShip(3, 3, "x", "Destroyer");
   expect(board.board[3][3].isShip).toBe("Destroyer");
   expect(board.board[3][4].isShip).toBe("Destroyer");
-
-  /*board.placeShip(2, 2, "y", "WarShip");
+});
+test("placeShip on y axis", () => {
   expect(board.board[2][2].isShip).toBe("WarShip");
-  expect(board.board[3][2].isShip).toBe("WarShip");*/
+  expect(board.board[3][2].isShip).toBe("WarShip");
+});
+board.recieveAttack(3, 3);
+test("recieveAttack, hits a ship", () => {
+  expect(board.board[3][3].isAttacked).toBe("hit");
+  expect(board.recieveAttack(3, 3)).toBe(2);
 });
